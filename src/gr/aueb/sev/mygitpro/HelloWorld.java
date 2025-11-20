68190d6c41e215b084116bc9e83051089efa6f7f
A.Kotsis <anastasiskotsis91@gmail.com>
2025-11-20 20:47:02 +0200

    initial commit

---
+++ src/gr/aueb/sev/mygitpro/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.sev.mygitpro;
+
+/**
+ * Hello world program
+ * For showing how to make it in a git project.
+ * How to stage and commit changes.
+ * And how to push to remote repository of GitHub
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
